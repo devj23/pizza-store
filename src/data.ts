a011b5c79887aa1b5e5bab8b31c3ed3722622275
Jeremy Barber <devj23@proton.me>
2023-11-07 16:24:19 -0800

    Initial Commit

---
+++ src/data.ts
@@ -0,0 +1,46 @@
+export const menuItems = [
+  {
+    id: 1,
+    title: "Cheese Pizza",
+    description: "Marinara sauce and Mozzarella Cheese.",
+    imgUrl: "/images/cheese-pizza.jpg",
+    category: "pizza",
+  },
+  {
+    id: 2,
+    title: "Pepperoni Pizza",
+    description: "Marinara sauce and pepperonis.",
+    imgUrl: "/images/pepperoni-pizza.jpg",
+    category: "pizza",
+  },
+  {
+    id: 3,
+    title: "Supreme Pizza",
+    description:
+      "Marinara sauce, basil, italian sausage, roma tomatoes, and olives.",
+    imgUrl: "/images/supreme-pizza.jpg",
+    category: "pizza",
+  },
+  {
+    id: 4,
+    title: "Cannoli",
+    description: "Ricotta-filled pastries with a crunchy shell.",
+    imgUrl: "/images/cannoli.jpg",
+    category: "dessert",
+  },
+  {
+    id: 5,
+    title: "Chocolate Chip Cookie",
+    description: "Soft and chewy chocolate chip cookies.",
+    imgUrl: "/images/cookies.jpg",
+    category: "dessert",
+  },
+  {
+    id: 6,
+    title: "Ice Cream Cone",
+    description:
+      "Vanilla and chocolate ice cream swirled in a golden waffle cone with sprinkles.",
+    imgUrl: "/images/ice-cream.jpg",
+    category: "dessert",
+  },
+];
