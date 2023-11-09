0ce8e2c2c22d2ed6a2e5c730920ec4939860fbcc
Jeremy Barber <devj23@proton.me>
2023-11-08 16:36:58 -0800

    Cleaned up files

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-
 const Hero = () => {
   return (
-    <section className="hero-section">
+    <section
+      className="hero-section"
+      style={{ backgroundImage: `url(images/hero-bg.jpg)` }}
+    >
       <div className="hero">
         <h1 className="hero-title">World&apos; best pizza!</h1>
         <p className="hero-lead">More cheese, more toppings, more to share.</p>
